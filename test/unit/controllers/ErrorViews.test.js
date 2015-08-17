c6cbde5e615308b8451cc362887669ab9b325dba
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-08-17 16:43:57 +1200

    testing more error views

--- test/unit/controllers/ErrorViews.test.js
+++ test/unit/controllers/ErrorViews.test.js
@@ -10,6 +10,13 @@ describe('Error Views', function() {
                 .get('/notfound')
                 .expect(404, done)
         });
+        it('should get model not found', function(done) {
+            req = request.agent(sails.hooks.http.app);
+            req
+                .get('/matters/-1')
+                .expect(404, done)
+        });
     });
+
 
 })
